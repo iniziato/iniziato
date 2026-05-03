cff0fac6d46459a49bc262ab85a6952ed3667a2e
Ana Menshikov <anamenshikov@ip-192-168-100-131.eu-west-1.compute.internal>
2026-05-03 22:34:28 +0400

    fix class naming

--- pages/classes/index.tsx
+++ pages/classes/index.tsx
@@ -140,7 +140,7 @@ export default function Classes() {
                     title: t("CLASS_FULL_BEGINNER"),
                     thumbnail: "/images/beginner.png",
                     duration: "10 წთ",
-                    src: "/videos/beginner3.mp4",
+                    src: "/videos/beginner2.mp4",
                 },
                 {
                     id: "3",
